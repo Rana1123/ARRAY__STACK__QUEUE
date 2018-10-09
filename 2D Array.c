43e8ef5e649fcfd006f2dfd838c91b44c4667a98
Rana1123 <42868564+Rana1123@users.noreply.github.com>
2018-10-09 23:02:30 +0600

    Add files via upload

---
+++ 2D Array.c
@@ -0,0 +1,41 @@
+//TwoDimensional Array
+
+#include<stdio.h>
+
+void main(){
+
+int a[10][10];
+
+int i,j,row, column;
+
+printf("Enter the rows number how many you want \n");
+
+scanf("%d", &row);//Have taken total rows number.
+
+printf("Enter the columns number how many you want \n");
+
+scanf("%d", &column);//Have taken total columns number
+
+printf("Enter your all Element one by one \n");
+
+for(i=0;i<row;i++){
+
+    for(j=0;j<column;j++){
+
+          scanf("%d", &a[i][j]);//Have taken total rows & columns from user.
+    }
+
+}
+printf("your array \n");
+
+for(i=0;i<row;i++){
+
+    for(j=0;j<column;j++){
+
+          printf("%d ", a[i][j]);//Shown all element which taken from user.
+    }
+    printf("\n");
+}
+
+
+}
